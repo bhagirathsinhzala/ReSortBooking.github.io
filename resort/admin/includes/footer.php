439955972f31446d7162915e78a3907c21ca8eb7
bhagirathsinhzala <bhagirathsinhzala6353@gmail.com>
2023-02-14 23:42:51 +0530

    ReSort Booking.

---
+++ resort/admin/includes/footer.php
@@ -0,0 +1,7 @@
+<div>
+         
+            <div class="footer">
+                    <div class="clearfix"> </div>
+                        <p style="font-family: cursive;">© 2022 Resort Booking Management System </p>
+            </div>
+        </div>
---
+++ resort/includes/footer.php
@@ -0,0 +1,8 @@
+<div class="footer-section">
+						<div class="container">
+							<div class="footer-top">
+								<p>Resort Booking Management Sytem @ 2022 </p>
+							</div>
+						
+				</div>
+			</div>
